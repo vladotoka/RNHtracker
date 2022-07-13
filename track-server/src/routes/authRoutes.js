f5041a4ff02421c8e43045d771a8fe33bcf06e52
vladotoka <vladimir.k.terziev@tutanota.com>
2022-07-13 22:08:11 +0300

    temp error handling

--- track-server/src/routes/authRoutes.js
+++ track-server/src/routes/authRoutes.js
@@ -7,15 +7,18 @@ const router = express.Router();
 router.post('/signup', async (req, res) => {
     //req.body contains an object with passed to request email an password
     const { email, password } = req.body;
+    try {
+        //creating new user instance
+        const user = new User({ email, password });
 
-    //creating new user instance
-    const user = new User({ email, password });
+        //saving user to mongoDB
+        await user.save();
 
-    //saving user to mongoDB
-    await user.save();
-
-    res.send(`New user was created with email: ${email}`);
-    console.log(`New user was created with email: ${email}`);
+        res.send(`New user was created with email: ${email}`);
+        console.log(`New user was created with email: ${email}`);
+    } catch (err) {
+        return res.status(422).send(err.message)
+    }
 });
 
 module.exports = router;
