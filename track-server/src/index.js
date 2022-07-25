ba9bda4b235151ccf3b7c6585534045006184aaa
vladotoka <vladimir.k.terziev@tutanota.com>
2022-07-25 16:00:32 +0300

    listing tracks

--- track-server/src/index.js
+++ track-server/src/index.js
@@ -5,14 +5,17 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes');
+const trackRoutes = require('./routes/trackRoutes');
 const requireAuth = require('./middlewares/requireAuth');
 
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
 app.use(authRoutes);
+app.use(trackRoutes);
 
+//keys.mongoDB === 'username:password'
 const mongoUri = `mongodb+srv://${keys.mongoDB}@cluster0.xtu0axj.mongodb.net/?retryWrites=true&w=majority`;
 
 mongoose.connect(mongoUri);
--- track-server/src/routes/trackRoutes.js
+++ track-server/src/routes/trackRoutes.js
@@ -7,3 +7,12 @@ const Track = mongoose.model('Track');
 const router = express.Router();
 
 router.use(requireAuth);
+
+router.get('/tracks', async (req, res, next) => {
+    const tracks = await Track.find({ userId: req.user._id });
+
+    res.send(tracks);
+    next(); // pass control to the next handler
+});
+
+module.exports = router;
